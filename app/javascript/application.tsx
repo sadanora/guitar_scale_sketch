28085f06e56f0e479c40a4533c8b3e1b339f9ea4
sadanora <sadanora.pi@gmail.com>
2023-05-01 15:46:47 +0900

    feat: ReactでHello,worldを表示する

---
+++ app/javascript/application.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const container = document.getElementById("root") as HTMLElement;
+const root = createRoot(container);
+
+document.addEventListener("DOMContentLoaded", () => {
+  root.render(
+    <React.StrictMode>
+      <App greeting="Hello" />
+    </React.StrictMode>
+  );
+});
