7633b525cabc9ed9acf0f18915990a74aabcc02f
sadanora <sadanora.pi@gmail.com>
2023-05-04 14:16:18 +0900

    feat: Fretモデルにバリデーションを追加する / fretboardモデルと関連付ける

--- app/models/fretboard.rb
+++ app/models/fretboard.rb
@@ -1,5 +1,5 @@
 class Fretboard < ApplicationRecord
   belongs_to :score
-  has_many :dots, dependent: :destroy
+  has_many :frets, dependent: :destroy
   validates :position, :start_fret, :end_fret, presence: true
 end
