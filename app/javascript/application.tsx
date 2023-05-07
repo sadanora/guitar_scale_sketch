3605eb86bda10cc96f7df149e066770a0637f2ba
sadanora <sadanora.pi@gmail.com>
2023-05-07 14:55:22 +0900

    style: 🚨 bin/lint

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-const container = document.getElementById("root") as HTMLElement;
+const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   root.render(
     <React.StrictMode>
       <App greeting="Hello" />
