06a8460b8eb078fee5a0ceea47dbea94cd209f62
sadanora <sadanora.pi@gmail.com>
2023-05-20 15:19:20 +0900

    feat: Scoreのscaffoldを追加
    $ bin/rails g scaffold Scores id:uuid title:string is_public:boolean score_code:text

---
+++ db/migrate/20230520052548_create_scores.rb
@@ -0,0 +1,11 @@
+class CreateScores < ActiveRecord::Migration[7.0]
+  def change
+    create_table :scores, id: :uuid do |t|
+      t.string :title, null: false
+      t.boolean :is_public, null: false
+      t.text :score_code, null: false
+
+      t.timestamps
+    end
+  end
+end
