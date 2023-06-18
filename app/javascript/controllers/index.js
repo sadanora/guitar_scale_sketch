80eb1091a4010d85a064c26e1b938ddf423812ff
sadanora <sadanora.pi@gmail.com>
2023-06-18 19:13:15 +0900

    bin/rails g stimulus score

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,10 +2,13 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application";
+import { application } from "./application"
 
-import FretboardController from "./fretboard_controller";
-application.register("fretboard", FretboardController);
+import FretboardController from "./fretboard_controller"
+application.register("fretboard", FretboardController)
 
-import HelloController from "./hello_controller";
-application.register("hello", HelloController);
+import HelloController from "./hello_controller"
+application.register("hello", HelloController)
+
+import ScoreController from "./score_controller"
+application.register("score", ScoreController)
