9bf8c9f8b1b88ecec6a5fe29a885c03b7dc42a8f
sadanora <sadanora.pi@gmail.com>
2023-08-02 14:33:56 +0900

    feat: ✨ show画面に指板図を表示する

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,10 +2,13 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application";
+import { application } from "./application"
 
-import HelloController from "./hello_controller";
-application.register("hello", HelloController);
+import HelloController from "./hello_controller"
+application.register("hello", HelloController)
 
-import ScoreController from "./score_controller";
-application.register("score", ScoreController);
+import ScoreController from "./score_controller"
+application.register("score", ScoreController)
+
+import ScoreFormController from "./score_form_controller"
+application.register("score-form", ScoreFormController)
