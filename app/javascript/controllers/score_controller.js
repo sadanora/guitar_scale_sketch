0bd2f92c81bdf8b146a8a4a6c75781824822c580
sadanora <sadanora.pi@gmail.com>
2023-08-09 12:26:51 +0900

    fix: 下端の指板図が見切れないようにする

--- app/javascript/controllers/score_controller.js
+++ app/javascript/controllers/score_controller.js
@@ -14,6 +14,7 @@ export default class extends Controller {
 
   draw() {
     const fretboardShapes = this.score.buildFretboardShapes();
+    this.score.setStageHeight();
     this.score.draw(fretboardShapes);
   }
 }
