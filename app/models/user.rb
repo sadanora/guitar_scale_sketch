1a1b74a8c8bc6e4aa4d7ebf5c86d132f649f5ce2
sadanora <sadanora.pi@gmail.com>
2023-08-11 20:45:10 +0900

    feat: ✨ googleログイン機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  def self.find_or_create_from_auth_hash!(auth_hash)
+    provider = auth_hash[:provider]
+    uid = auth_hash[:uid]
+    name = auth_hash[:info][:name]
+    image_url = auth_hash[:info][:image]
+
+    User.find_or_create_by!(provider: provider, uid: uid) do |user|
+      user.name = name
+      user.image_url = image_url
+    end
+  end
 end
