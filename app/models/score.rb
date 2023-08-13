dfb48fcff84a67e558de2071237bc65a20e0f395
sadanora <sadanora.pi@gmail.com>
2023-08-13 13:53:36 +0900

    feat: Scoreモデルにバリデーションを追加

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Score < ApplicationRecord
-  validates :title, presence: true
+  validates :title, length: { maximum: 50 }, presence: true
   validates :score_code, presence: true
+  validates :is_public, presence: true
 end
