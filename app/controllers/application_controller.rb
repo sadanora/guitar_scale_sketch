41b0b7a24bf5d4326b1256a1103fe4e32fd49907
sadanora <sadanora.pi@gmail.com>
2023-08-17 08:46:49 +0900

    feat: ✨ Score一覧にログイン中のユーザーが作成したscoreのみを表示する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate
   helper_method %i[logged_in? current_user]
 
   private
@@ -10,6 +11,12 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def authenticate
+    return if logged_in?
+    redirect_to root_path, alert: "ログインしてください"
   end
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Score < ApplicationRecord
+  belongs_to :user
   validates :title, length: { maximum: 50 }, presence: true
   validates :score_code, presence: true
   validates :is_public, presence: true
