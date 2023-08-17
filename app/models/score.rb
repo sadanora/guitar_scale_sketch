97d1741a4bcffd5a0e426677d1a97b1bb4c6385d
sadanora <sadanora.pi@gmail.com>
2023-08-17 11:31:58 +0900

    fix: 非公開のscoreを保存できるようにする

--- app/models/score.rb
+++ app/models/score.rb
@@ -4,5 +4,4 @@ class Score < ApplicationRecord
   belongs_to :user
   validates :title, length: { maximum: 50 }, presence: true
   validates :score_code, presence: true
-  validates :is_public, presence: true
 end
