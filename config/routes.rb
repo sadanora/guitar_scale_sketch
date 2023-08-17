8ccb186715e3b057d5946c63bb99bf3eaa9be83b
sadanora <sadanora.pi@gmail.com>
2023-08-17 17:33:10 +0900

    feat: ✨ ユーザーの退会処理を実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :scores
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
+  resource :retirements
 
   root 'welcome#index'
 end
