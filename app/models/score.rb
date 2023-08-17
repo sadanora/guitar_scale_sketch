cd2b006c517cbec5dcefbde3c977870047cd789a
sadanora <sadanora.pi@gmail.com>
2023-08-17 12:13:54 +0900

    feat: ✨ 非公開のscoreは作成者しか表示できないようにする

--- app/models/score.rb
+++ app/models/score.rb
@@ -4,4 +4,9 @@ class Score < ApplicationRecord
   belongs_to :user
   validates :title, length: { maximum: 50 }, presence: true
   validates :score_code, presence: true
+
+  def created_by?(user)
+    return false unless user
+    user_id == user.id
+  end
 end
