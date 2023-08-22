b7698d73524b0cec7f76a2d50c45965b174fabb4
sadanora <sadanora.pi@gmail.com>
2023-08-22 09:14:33 +0900

    feat: ✅  Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@
 class User < ApplicationRecord
   has_many :created_scores, class_name: 'Score', dependent: :destroy
 
+  validates :provider, presence: true
+  validates :uid, presence: true
+  validates :name, presence: true
+  validates :image_url, presence: true
+
+  validates :uid, uniqueness: { scope: :provider }
+
   def self.find_or_create_from_auth_hash!(auth_hash)
     provider = auth_hash[:provider]
     uid = auth_hash[:uid]
