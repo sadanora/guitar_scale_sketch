1fde1d72e9ebfd661748230043d1894b04af6966
sadanora <sadanora.pi@gmail.com>
2023-08-27 21:51:58 +0900

    test: ✅  googleログインのテストをできるようにする

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     provider { "google_oauth2" }
-    uid { "123456789123456789123" }
+    sequence(:uid) { |n| "123456789123456789123#{n}" }
     name { "Ritchie" }
     image_url { "http://example.com/image1.jpg" }
 
