06fb7e1bbe8bf778defa82ba78937b4f406e53ff
sadanora <sadanora.pi@gmail.com>
2023-08-28 10:30:54 +0900

    style: 🚨 lint

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
-    provider { "google_oauth2" }
+    provider { 'google_oauth2' }
     sequence(:uid) { |n| "123456789123456789123#{n}" }
-    name { "Ritchie" }
-    image_url { "http://example.com/image1.jpg" }
+    name { 'Ritchie' }
+    image_url { 'http://example.com/image1.jpg' }
 
     trait :with_scores do
-      after(:create) { |user| create_list(:score, 5, user: user)}
+      after(:create) { |user| create_list(:score, 5, user:) }
     end
   end
 end
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,4 +1,6 @@
-RSpec.configure do | config |
+# frozen_string_literal: true
+
+RSpec.configure do |config|
   config.before(:each, type: :system, js: true) do
     driven_by :selenium_chrome_headless
   end
