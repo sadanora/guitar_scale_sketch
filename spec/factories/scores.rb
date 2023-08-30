7fae92affdff198c4261dbdbdfff6f8e408788db
sadanora <sadanora.pi@gmail.com>
2023-08-30 23:15:45 +0900

    test: ✅  ScoreのCRUDのシステムスペックを追加

--- spec/factories/scores.rb
+++ spec/factories/scores.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :score do
-    sequence(:title) { |n| "test scale #{n}" }
+    sequence(:title) { |n| "#{user.name}'s scale #{n}" }
     is_public { true }
     score_code { '[{"position":1,"startFret":1,"endFret":7,"dots":[{"fill":"#C7243A","fret":1,"guitarString":1}]}]' }
     association :user
