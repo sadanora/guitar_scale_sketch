2114798211ddbd8f93c080e9aeef56b26a8854ee
sadanora <sadanora.pi@gmail.com>
2023-09-12 13:37:22 +0900

    test: テストのブラウザサイズを指定する

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,6 +2,6 @@
 
 RSpec.configure do |config|
   config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome_headless
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
   end
 end
