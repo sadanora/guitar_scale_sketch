cf3fccebcfbd0cad4ba119f21e464677b02ba3ee
sadanora <sadanora.pi@gmail.com>
2023-10-04 22:53:53 +0900

    test: 🔥 ユーザーが複数の指板図を作れるかmodel specでテストしないように

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,4 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'has many fingerings' do
-    user = FactoryBot.create(:user, :with_fingerings)
-    expect(user.created_fingerings.length).to eq 5
-  end
 end
