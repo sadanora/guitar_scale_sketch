fd6f634975351f1959a4bcefa48b3eed43edc4fb
sadanora <sadanora.pi@gmail.com>
2023-10-06 15:20:59 +0900

    test: ✅ Fingering#created_by?のテストを追加

--- spec/models/fingering_spec.rb
+++ spec/models/fingering_spec.rb
@@ -3,4 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe Fingering, type: :model do
+  let!(:fingering) { FactoryBot.create(:fingering) }
+
+  describe '#created_by?' do
+    it 'return true created user' do
+      created_user = fingering.user
+      expect(fingering.created_by?(created_user)).to be true
+    end
+
+    it 'return false not created user' do
+      not_created_user = FactoryBot.create(:user)
+      expect(fingering.created_by?(not_created_user)).to be false
+    end
+
+    it 'return false unless user' do
+      fingering = FactoryBot.create(:fingering)
+      expect(fingering.created_by?(nil)).to be false
+    end
+  end
 end
