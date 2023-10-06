9f1f51b6aaa0f44f532a5e4046ebb7afccc7c29a
sadanora <sadanora.pi@gmail.com>
2023-10-06 13:12:45 +0900

    test: ✅ User.find_or_create_from_auth_hash!のテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe '.find_or_create_from_auth_hash!' do
+    it 'find valid user' do
+      user = FactoryBot.create(:user)
+      auth_hash = {
+        provider: user.provider,
+        uid: user.uid,
+        info: {
+          name: user.name,
+          image: user.image_url,
+        }
+      }
+      expect(User.find_or_create_from_auth_hash!(auth_hash)).to eq user
+    end
+  end
 end
