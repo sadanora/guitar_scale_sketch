2cb3efca5ca9db37a5877d6ce0be4aac6ab46951
sadanora <sadanora.pi@gmail.com>
2023-10-19 16:31:28 +0900

    refactor: 不要なroutingの記述を削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
     get '/(*path)', to: redirect { |path_params,| "https://guitar-scale-sketch.com/#{path_params[:path]}" }
   end
 
-  get 'welcome/index'
+  root 'welcome#index'
+
   get 'privacy_policy', to: 'welcome#privacy_policy'
   get 'tos', to: 'welcome#tos'
   resources :fingerings
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
-  resource :retirements, only: [:new, :create, :destroy]
-
-  root 'welcome#index'
+  resource :retirements, only: [:new, :create]
 end
