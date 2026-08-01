8590d12c14a3366241b2e15805b33a8658631e54
sadanora <sadanora.pi@gmail.com>
2026-08-01 19:44:21 +0900

    upgrade rails7.1.6

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,20 @@ module GuitarScaleSketch
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # タイムゾーンをTokyo（日本）にする
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
+        # タイムゾーンをTokyo（日本）にする
     config.time_zone = "Tokyo"
     # デフォルトのロケールを日本にする
     config.i18n.default_locale = :ja
